389cd4452e6face4f81588a562cba07212051a9c
mehakb7 <mehak@founderandlightning.com>
2025-01-19 15:05:36 +0530

    added habitModal, constants and lang

--- components/organism/HabitModal/HabitModal.tsx
+++ components/organism/HabitModal/HabitModal.tsx
@@ -17,6 +17,8 @@ import { Label } from "@/components/ui/label";
 import RadioButton from "@/components/molecules/radioButton";
 import SelectBox from "@/components/molecules/selectBox";
 import { ErrorMessage } from "@/components/ui/errorMessage";
+import { lang } from '@/lib/lang';
+import { TARGET_TYPE_OPTIONS, COLOR_OPTIONS } from '@/lib/constants';
 
 export function CreateHabitModal() {
   const {
@@ -25,14 +27,14 @@ export function CreateHabitModal() {
     formState: { errors },
     watch,
   } = useForm({
-    defaultValues:{
-      name:"",
-      question:"",
+    defaultValues: {
+      name: "",
+      question: "",
       unit: "",
       target: 0,
       targetType: "At Most" as "At Most" | "At Least",
       isMeasurable: "no" as "yes" | "no",
-      color: "rose"
+      color: "rose",
     },
     resolver: zodResolver(schema),
   });
@@ -46,131 +48,124 @@ export function CreateHabitModal() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">Create Habit</Button>
+        <Button variant="outline">{lang.CREATE_HABIT_BUTTON}</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
-          <DialogTitle>Create Habit </DialogTitle>
+          <DialogTitle>{lang.CREATE_HABIT_TITLE}</DialogTitle>
           <DialogDescription>
-            Create a new habit to track your progress.
+            {lang.CREATE_HABIT_DESCRIPTION}
           </DialogDescription>
         </DialogHeader>
         <div className="flex items-center space-x-2">
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 w-full">
             <div>
-              <Label htmlFor="name">Habit Name</Label>
+              <Label htmlFor="name">{lang.HABIT_NAME_LABEL}</Label>
               <Controller
                 name="name"
                 control={control}
-                render={({ field }) => <Input id="name" {...field} placeholder="eg:Walk 10,000 steps" />}
+                render={({ field }) => <Input id="name" {...field} placeholder={lang.HABIT_NAME_PLACEHOLDER} />}
               />
-              {errors.name && <ErrorMessage message={"" + errors?.name?.message}/> }
+              {errors.name && <ErrorMessage message={"" + errors?.name?.message} />}
             </div>
 
             <div>
-              <Label htmlFor="description">Description</Label>
+              <Label htmlFor="description">{lang.HABIT_DESCRIPTION_LABEL}</Label>
               <Controller
                 name="question"
                 control={control}
-                render={({ field }) => <Input id="question" {...field} placeholder="eg: Am I healthy" />}
+                render={({ field }) => <Input id="question" {...field} placeholder={lang.HABIT_DESCRIPTION_PLACEHOLDER} />}
               />
               {errors.question && (
-                <ErrorMessage message={"" + errors?.question?.message}/> 
+                <ErrorMessage message={"" + errors?.question?.message} />
               )}
             </div>
 
             <div>
-              <label>Is it a measurable habit?</label>
+              <label>{lang.IS_MEASURABLE_LABEL}</label>
               <Controller
                 name="isMeasurable"
                 control={control}
                 render={({ field }) => (
-                 <RadioButton 
-                 onValueChange={field.onChange}
-                 defaultValue={field.value}
-                  options={[
-                    {value: "yes", label: "Yes"},
-                    {value: "no", label: "No"}
-                  ]}
-                  className="flex"
+                  <RadioButton
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    options={[
+                      { value: "yes", label: lang.YES_OPTION },
+                      { value: "no", label: lang.NO_OPTION },
+                    ]}
+                    className="flex"
                   />
                 )}
               />
               {errors.isMeasurable && (
-                <ErrorMessage message={"" + errors?.isMeasurable?.message}/> 
+                <ErrorMessage message={"" + errors?.isMeasurable?.message} />
               )}
             </div>
 
             {isMeasurable && (
               <>
                 <div>
-                  <Label htmlFor="unit">Unit</Label>
+                  <Label htmlFor="unit">{lang.UNIT_LABEL}</Label>
                   <Controller
                     name="unit"
                     control={control}
-                    render={({ field }) => <Input id="unit" {...field} placeholder="eg: mile" />}
+                    render={({ field }) => <Input id="unit" {...field} placeholder={lang.UNIT_PLACEHOLDER} />}
                   />
                   {errors.unit && (
-                <ErrorMessage message={"" + errors?.unit?.message}/> 
-              )}
+                    <ErrorMessage message={"" + errors?.unit?.message} />
+                  )}
                 </div>
                 <div>
-                  <Label htmlFor="unit">Target</Label>
+                  <Label htmlFor="target">{lang.TARGET_LABEL}</Label>
                   <Controller
                     name="target"
                     control={control}
                     render={({ field }) => (
-                      <Input type="number" id="target" {...field} placeholder="eg: 50" />
-                    )} 
+                      <Input type="number" id="target" {...field} placeholder={lang.TARGET_PLACEHOLDER} />
+                    )}
                   />
-                  {errors.target &&<ErrorMessage message={"" + errors?.target?.message}/> }
+                  {errors.target && <ErrorMessage message={"" + errors?.target?.message} />}
                 </div>
                 <div>
-                  <Label htmlFor="unit">Target Type</Label>
+                  <Label htmlFor="targetType">{lang.TARGET_TYPE_LABEL}</Label>
                   <Controller
                     name="targetType"
                     control={control}
                     render={({ field }) => (
-                      <SelectBox 
-                      value={field.value||""}
-                      placeholder={"Select Target Type"} 
-                       error={Boolean(errors.targetType)} errorMessage={""+errors?.targetType?.message||""}
-                       onValueChange={field.onChange}
-                        options={[
-                          {value: "At Least", label: "At Least"},
-                          {value: "At Most", label: "At Most"},
-                        ]}>
-                      </SelectBox>
+                      <SelectBox
+                        value={field.value || ""}
+                        placeholder={lang.SELECT_TARGET_TYPE_PLACEHOLDER}
+                        error={Boolean(errors.targetType)}
+                        errorMessage={"" + errors?.targetType?.message || ""}
+                        onValueChange={field.onChange}
+                        options={TARGET_TYPE_OPTIONS}
+                      />
                     )}
                   />
-                 
                 </div>
               </>
             )}
 
             <div>
-              <Label htmlFor="color">Color</Label>
+              <Label htmlFor="color">{lang.COLOR_LABEL}</Label>
               <Controller
                 name="color"
                 control={control}
-                render={({ field }) =>
-                  <SelectBox 
-                value={field.value||""}
-                placeholder={"Select Target Type"} 
-                 error={Boolean(errors.targetType)} errorMessage={""+errors?.color?.message||""}
-                 onValueChange={field.onChange}
-                 className={field.value? `bg-${field.value}-500` : ""}
-                  options={[
-                    {value: "rose", label: "", className:"bg-rose-500 py-2 border focus:bg-rose-200 focus:opacity-70"},
-                    {value: "indigo", label: "", className:"bg-indigo-500 py-2  focus:bg-indigo-200 "},
-                    {value: "green", label: "", className:"bg-green-500 py-2 focus:bg-green-200 "},
-                    {value: "purple", label: "", className:"bg-purple-500 py-2 focus:bg-purple-200 "},
-                    {value: "yellow", label: "", className:"bg-yellow-500 py-2 focus:bg-yellow-200 "},
-                  ]}>
-                </SelectBox>}
+                render={({ field }) => (
+                  <SelectBox
+                    value={field.value || ""}
+                    placeholder={lang.SELECT_TARGET_TYPE_PLACEHOLDER}
+                    error={Boolean(errors.color)}
+                    errorMessage={"" + errors?.color?.message || ""}
+                    onValueChange={field.onChange}
+                    className={field.value ? `bg-${field.value}-500` : ""}
+                    options={COLOR_OPTIONS}
+                  />
+                )}
               />
             </div>
-            <Button type="submit">Create Habit</Button>
+            <Button type="submit">{lang.SUBMIT_BUTTON}</Button>
           </form>
         </div>
       </DialogContent>
---
+++ lib/constants.ts
@@ -0,0 +1,32 @@
+export const TARGET_TYPE_OPTIONS = [
+  { value: "At Least", label: "At Least" },
+  { value: "At Most", label: "At Most" },
+];
+
+export const COLOR_OPTIONS = [
+  {
+    value: "rose",
+    label: "",
+    className: "bg-rose-500 py-2 border focus:bg-rose-200 focus:opacity-70",
+  },
+  {
+    value: "indigo",
+    label: "",
+    className: "bg-indigo-500 py-2 focus:bg-indigo-200",
+  },
+  {
+    value: "green",
+    label: "",
+    className: "bg-green-500 py-2 focus:bg-green-200",
+  },
+  {
+    value: "purple",
+    label: "",
+    className: "bg-purple-500 py-2 focus:bg-purple-200",
+  },
+  {
+    value: "yellow",
+    label: "",
+    className: "bg-yellow-500 py-2 focus:bg-yellow-200",
+  },
+];
