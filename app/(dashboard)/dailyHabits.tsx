c0819636d937a25a0dac964a0f4984889b70f562
mehakb7 <mehak@founderandlightning.com>
2025-01-19 14:59:12 +0530

    added create habit Modal

--- app/(dashboard)/dailyHabits.tsx
+++ app/(dashboard)/dailyHabits.tsx
@@ -1,13 +1,19 @@
 "use client";
-import React from 'react';
-import { lang } from "@/lib/lang";
+import React from 'react'
+import {lang} from '@/lib/lang';
+import { CreateHabitModal } from '@/components/organism/HabitModal/HabitModal';
+
 
 const DailyHabits = () => {
   return (
-    <div className='container mx-auto'>
-      <h1>{lang.APP_TITLE}</h1>
+    <div className='container mx-auto mt-4'>
+        <div className='flex justify-between items-center'>
+        <h1 className='text-lg font-bold'>{lang.APP_TITLE}</h1>
+        <CreateHabitModal/>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default DailyHabits;
+
+export default DailyHabits
